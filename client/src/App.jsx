bb509bb1ca9e9df373eecb1920babd0c938a99f4
mikitavydrankou <mikitavydrankou@gmail.com>
2025-10-10 13:32:52 +0200

    frontend mini refactor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,55 @@
 import { CssBaseline, Container, Box } from "@mui/material";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import { useEffect } from "react";
 import HomePage from "./pages/Homepage";
 import SigninPage from "./pages/authPages/SigninPage";
 import SignupPage from "./pages/authPages/SignupPage";
 import TopNavbar from "./components/navbars/TopNavbar";
 import BottomNavbar from "./components/navbars/BottomNavbar";
-
 import OfferPage from "./pages/OfferPage";
 import { CreateOfferPage } from "./pages/CreateOfferPage";
 import { useAuthStore } from "./store/authStore";
 import InfoPage from "./pages/InfoPage";
 
-const MainLayout = () => {
-    return (
-        <Box
+const ROUTES = {
+    HOME: "/",
+    SIGNIN: "/signin",
+    SIGNUP: "/signup",
+    OFFER: "/offer/:id",
+    OFFER_CREATE: "/offer/create",
+    PROFILE: "/profile",
+};
+
+const MainLayout = () => (
+    <Box
+        sx={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100vh",
+            bgcolor: "background.default",
+            color: "text.primary",
+            background: `url('/topography.svg')`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+        }}
+    >
+        <TopNavbar />
+        <Container
+            component="main"
             sx={{
-                display: "flex",
-                flexDirection: "column",
-                minHeight: "100vh",
-                bgcolor: "background.default",
-                color: "text.primary",
-                background: `url('/topography.svg')`,
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat",
-                backgroundPosition: "center",
+                flex: 1,
+                py: 4,
+                mt: { xs: "56px", sm: "64px" },
+                mb: { xs: "56px", sm: "64px" },
+                px: { xs: 2, sm: 3 },
             }}
         >
-            <TopNavbar />
-            <Container
-                component="main"
-                sx={{
-                    flex: 1,
-                    py: 4,
-                    mt: { xs: "56px", sm: "64px" },
-                    mb: { xs: "56px", sm: "64px" },
-                    px: { xs: 2, sm: 3 },
-                }}
-            >
-                <Outlet />
-            </Container>
-            <BottomNavbar />
-        </Box>
-    );
-};
+            <Outlet />
+        </Container>
+        <BottomNavbar />
+    </Box>
+);
 
 function App() {
     const checkAuth = useAuthStore((state) => state.checkAuth);
@@ -56,14 +62,15 @@ function App() {
         <>
             <CssBaseline />
             <Routes>
-                <Route path="/signin" element={<SigninPage />} />
-                <Route path="/signup" element={<SignupPage />} />
-                <Route path="/offer/:id" element={<OfferPage />} />
+                <Route path={ROUTES.SIGNIN} element={<SigninPage />} />
+                <Route path={ROUTES.SIGNUP} element={<SignupPage />} />
+                <Route path={ROUTES.OFFER} element={<OfferPage />} />
 
                 <Route element={<MainLayout />}>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/profile" element={<InfoPage />} />
-                    <Route path="/offer/create" element={<CreateOfferPage />} />
+                    <Route path={ROUTES.HOME} element={<HomePage />} />
+                    <Route path={ROUTES.PROFILE} element={<InfoPage />} />
+                    <Route path={ROUTES.OFFER_CREATE} element={<CreateOfferPage />} />
+                    <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
                 </Route>
             </Routes>
         </>
