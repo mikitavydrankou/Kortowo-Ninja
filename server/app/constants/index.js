42fc7f85e26fdad3a9b3a58f7ed0040c9f3af0fd
mikitavydrankou <mikitavydrankou@gmail.com>
2025-10-10 13:03:27 +0200

    refactored constants

---
+++ server/app/constants/index.js
@@ -0,0 +1,39 @@
+export const ROLES = {
+    USER: "user",
+    ADMIN: "admin",
+    MODERATOR: "moderator",
+};
+
+export const ROLE_HIERARCHY = {
+    [ROLES.USER]: 1,
+    [ROLES.MODERATOR]: 2,
+    [ROLES.ADMIN]: 3,
+};
+
+export const OFFER_STATUS = {
+    ACTIVE: "active",
+    ARCHIVED: "archived",
+};
+
+export const PLACES = [
+    "DS1",
+    "DS2",
+    "DS3",
+    "DS4",
+    "DS6",
+    "DS7",
+    "DS8",
+    "DS9",
+    "DS119",
+    "Górka Kortowska",
+];
+
+export const COOKIE_SETTINGS = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+    maxAge: 86400 * 1000, // 24 hours
+};
+
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/;
+export const FACEBOOK_URL_REGEX = /^(https?:\/\/)?(www\.|m\.)?(facebook|fb)\.com\/[\S]+$/i;
