049196a53ebafeb91316dc2071397d9e3cee94ac
mikitavydrankou <mikitavydrankou@gmail.com>
2025-10-10 13:04:48 +0200

    error handler for middleware

---
+++ server/app/middleware/errorHandler.js
@@ -0,0 +1,33 @@
+import logger from "../config/logger.js";
+import { AppError } from "../utils/errors.js";
+import db from "../models/index.js";
+
+export const errorHandler = (err, req, res, next) => {
+    logger.error(err.message, {
+        stack: err.stack,
+        path: req.path,
+        method: req.method,
+    });
+
+    if (err instanceof db.Sequelize.ValidationError) {
+        return res.status(400).json({
+            message: err.errors.map((e) => e.message).join(", "),
+        });
+    }
+
+    if (err instanceof db.Sequelize.UniqueConstraintError) {
+        return res.status(400).json({
+            message: err.errors.map((e) => e.message).join(", "),
+        });
+    }
+
+    if (err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            message: err.message,
+        });
+    }
+
+    res.status(500).json({
+        message: "Internal server error",
+    });
+};
